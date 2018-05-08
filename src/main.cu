1689424ff2eaa47bb3da0197442dbcb3b0d38c7d
Brenan Keane Balbido <bbalbido@127x07.csc.calpoly.edu>
2018-05-07 22:41:09 -0700

    Added helper functions for filling out initial conditions for path volume filling

--- src/main.cu
+++ src/main.cu
@@ -331,6 +331,116 @@ void setBigDiffVolumeParallel(struct FloatVolume *fv, struct Picture *picture1,
     subfloatvolume_size * sizeof(float));
 }
 
+// Fill cells of path volume where x = 0 and y = 0
+void setX0Y0(struct FloatVolume *pv, struct FloatVolume *dv) {
+  unsigned i;
+  for(i = 1; i < pv->depth; i++) {
+    *(pv->contents + toIndex3D(0, 0, pv->width, i, pv->depth)) =
+      *(dv->contents + toIndex3D(0, 0, pv->width, i, pv->depth)) +
+      *(pv->contents + toIndex3D(0, 0, pv->width, i - 1, pv->depth));
+  }
+}
+
+// Fill cells where z = 0 and y = 0
+void setZ0Y0(struct FloatVolume *pv, struct FloatVolume *dv) {
+  unsigned i;
+  for(i = 1; i < pv->width; i++) {
+    *(pv->contents + toIndex3D(0, i, pv->width, 0, pv->depth)) =
+      *(dv->contents + toIndex3D(0, i, pv->width, 0, pv->depth)) +
+      *(pv->contents + toIndex3D(0, i - 1, pv->width, 0, pv->depth));
+  }
+}
+
+// Fill cells where z = 0 and x = 0
+void setZ0X0(struct FloatVolume *pv, struct FloatVolume *dv) {
+  unsigned i;
+  for(i = 1; i < pv->height; i++) { 
+    *(pv->contents + toIndex3D(i, 0, pv->width, 0, pv->depth)) =
+      *(dv->contents + toIndex3D(i, 0, pv->width, 0, pv->depth)) +
+      *(pv->contents + toIndex3D(i - 1, 0, pv->width, 0, pv->depth));
+  }
+}
+
+// Fill cells where x = 0, assuming Z0X0 and Z0Y0 are filled
+void setX0(struct FloatVolume *pv, struct FloatVolume *dv) {
+  float candidates2D[3], minCandidate;
+  unsigned i, j;
+  for(i = 1; i < pv->height; i++) { 
+    for(j = 1; j < pv->depth; j++) {
+      candidates2D[0] =
+        *(pv->contents + toIndex3D(i, 0, pv->width, j - 1, pv->depth));
+      candidates2D[1] =
+        *(pv->contents + toIndex3D(i - 1, 0, pv->width, j - 1, pv->depth));
+      candidates2D[2] =
+        *(pv->contents + toIndex3D(i - 1, 0, pv->width, j, pv->depth));
+
+      minCandidate = candidates2D[0];
+      if(candidates2D[1] < minCandidate)
+        minCandidate = candidates2D[1];
+      if(candidates2D[2] < minCandidate)
+        minCandidate = candidates2D[2];
+
+      *(pv->contents + toIndex3D(i, 0, pv->width, j, pv->depth)) =
+        *(dv->contents + toIndex3D(i, 0, pv->width, j, pv->depth)) +
+        minCandidate;
+    }
+  }
+}
+
+// Fill cells where y = 0, assuming Z0Y0 and X0Y0 are filled
+void setY0(struct FloatVolume *pv, struct FloatVolume *dv) {
+  float candidates2D[3], minCandidate;
+  unsigned i, j;
+
+  for(i = 1; i < pv->width; i++) {
+    for(j = 1; j < pv->depth; j++) {
+      candidates2D[0] =
+        *(pv->contents + toIndex3D(0, i, pv->width, j - 1, pv->depth));
+      candidates2D[1] =
+        *(pv->contents + toIndex3D(0, i - 1, pv->width, j - 1, pv->depth));
+      candidates2D[2] =
+        *(pv->contents + toIndex3D(0, i - 1, pv->width, j, pv->depth));
+
+      minCandidate = candidates2D[0];
+      if(candidates2D[1] < minCandidate)
+        minCandidate = candidates2D[1];
+      if(candidates2D[2] < minCandidate)
+        minCandidate = candidates2D[2];
+
+      *(pv->contents + toIndex3D(0, i, pv->width, j, pv->depth)) =
+        *(dv->contents + toIndex3D(0, i, pv->width, j, pv->depth)) +
+        minCandidate;
+    }
+  }
+}
+
+// Fill cells where z = 0, assuming Z0Y0 and Z0X0 are filled
+void setZ0(struct FloatVolume *pv, struct FloatVolume *dv) {
+  float candidates2D[3], minCandidate;
+  unsigned i, j;
+
+  for(i = 1; i < pv->height; i++) {
+    for(j = 1; j < pv->width; j++) {
+      candidates2D[0] =
+        *(pv->contents + toIndex3D(i, j - 1, pv->width, 0, pv->depth));
+      candidates2D[1] =
+        *(pv->contents + toIndex3D(i - 1, j - 1, pv->width, 0, pv->depth));
+      candidates2D[2] =
+        *(pv->contents + toIndex3D(i - 1, j, pv->width, 0, pv->depth));
+
+      minCandidate = candidates2D[0]; 
+      if(candidates2D[1] < minCandidate)
+        minCandidate = candidates2D[1];
+      if(candidates2D[2] < minCandidate)
+        minCandidate = candidates2D[2];
+
+      *(pv->contents + toIndex3D(i, j, pv->width, 0, pv->depth)) =
+        *(dv->contents + toIndex3D(i, j, pv->width, 0, pv->depth)) +
+        minCandidate;
+    }
+  }
+}
+
 // Function too thicc
 void pathVolumeInit(struct FloatVolume *pv, struct FloatVolume *dv) {
   int fvDataLen;
@@ -355,91 +465,22 @@ void pathVolumeInit(struct FloatVolume *pv, struct FloatVolume *dv) {
   *(pv->contents + 0) = *(dv->contents + 0);
 
   // Fill cells where x = 0 and y = 0
-  for(i = 1; i < pv->depth; i++) {
-    *(pv->contents + toIndex3D(0, 0, pv->width, i, pv->depth)) =
-      *(dv->contents + toIndex3D(0, 0, pv->width, i, pv->depth)) +
-      *(pv->contents + toIndex3D(0, 0, pv->width, i - 1, pv->depth));
-  }
+  setX0Y0(pv, dv);
 
   // Fill cells where z = 0 and y = 0
-  for(i = 1; i < pv->width; i++) {
-    *(pv->contents + toIndex3D(0, i, pv->width, 0, pv->depth)) =
-      *(dv->contents + toIndex3D(0, i, pv->width, 0, pv->depth)) +
-      *(pv->contents + toIndex3D(0, i - 1, pv->width, 0, pv->depth));
-  }
+  setZ0Y0(pv, dv);
 
   // Fill cells where z = 0 and x = 0
-  for(i = 1; i < pv->height; i++) {
-    *(pv->contents + toIndex3D(i, 0, pv->width, 0, pv->depth)) =
-      *(dv->contents + toIndex3D(i, 0, pv->width, 0, pv->depth)) +
-      *(pv->contents + toIndex3D(i - 1, 0, pv->width, 0, pv->depth));
-  }
+  setZ0X0(pv, dv);
 
   // Fill cells where x = 0
-  for(i = 1; i < pv->height; i++) {
-    for(j = 1; j < pv->depth; j++) {
-      candidates2D[0] =
-        *(pv->contents + toIndex3D(i, 0, pv->width, j - 1, pv->depth));
-      candidates2D[1] =
-        *(pv->contents + toIndex3D(i - 1, 0, pv->width, j - 1, pv->depth));
-      candidates2D[2] =
-        *(pv->contents + toIndex3D(i - 1, 0, pv->width, j, pv->depth));
-
-      minCandidate = candidates2D[0];
-      if(candidates2D[1] < minCandidate)
-        minCandidate = candidates2D[1];
-      if(candidates2D[2] < minCandidate)
-        minCandidate = candidates2D[2];
-
-      *(pv->contents + toIndex3D(i, 0, pv->width, j, pv->depth)) =
-        *(dv->contents + toIndex3D(i, 0, pv->width, j, pv->depth)) +
-        minCandidate;
-    }
-  }
+  setX0(pv, dv);
 
   // Fill cells where y = 0
-  for(i = 1; i < pv->width; i++) {
-    for(j = 1; j < pv->depth; j++) {
-      candidates2D[0] =
-        *(pv->contents + toIndex3D(0, i, pv->width, j - 1, pv->depth));
-      candidates2D[1] =
-        *(pv->contents + toIndex3D(0, i - 1, pv->width, j - 1, pv->depth));
-      candidates2D[2] =
-        *(pv->contents + toIndex3D(0, i - 1, pv->width, j, pv->depth));
-
-      minCandidate = candidates2D[0];
-      if(candidates2D[1] < minCandidate)
-        minCandidate = candidates2D[1];
-      if(candidates2D[2] < minCandidate)
-        minCandidate = candidates2D[2];
-
-      *(pv->contents + toIndex3D(0, i, pv->width, j, pv->depth)) =
-        *(dv->contents + toIndex3D(0, i, pv->width, j, pv->depth)) +
-        minCandidate;
-    }
-  }
+  setY0(pv, dv);
 
   // Fill cells where z = 0
-  for(i = 1; i < pv->height; i++) {
-    for(j = 1; j < pv->width; j++) {
-      candidates2D[0] =
-        *(pv->contents + toIndex3D(i, j - 1, pv->width, 0, pv->depth));
-      candidates2D[1] =
-        *(pv->contents + toIndex3D(i - 1, j - 1, pv->width, 0, pv->depth));
-      candidates2D[2] =
-        *(pv->contents + toIndex3D(i - 1, j, pv->width, 0, pv->depth));
-
-      minCandidate = candidates2D[0];
-      if(candidates2D[1] < minCandidate)
-        minCandidate = candidates2D[1];
-      if(candidates2D[2] < minCandidate)
-        minCandidate = candidates2D[2];
-
-      *(pv->contents + toIndex3D(i, j, pv->width, 0, pv->depth)) =
-        *(dv->contents + toIndex3D(i, j, pv->width, 0, pv->depth)) +
-        minCandidate;
-    }
-  }
+  setZ0(pv, dv);
 }
 
 void setPathVolumeSerial(struct FloatVolume *pv, struct FloatVolume *dv) {
@@ -635,6 +676,42 @@ void setPathVolumeParallel(struct FloatVolume *pv, struct FloatVolume *dv) {
   /* num_blocks = ((fv->height - 1) / 10 + ((fv->height - 1) % 10)) *
     ((fv->width - 1) / 10 + ((fv->width - 1) % 10)) *
     ((fv->depth - 1) / 10 + ((fv->depth - 1) % 10)); */
+}
+
+void makeEmptyFloatVolume(struct FloatVolume *fv, unsigned height,
+  unsigned width, unsigned depth) {
+  unsigned fvDataLen, i;
+
+  fv->height = height;
+  fv->width = width;
+  fv->depth = depth;
+
+  fvDataLen = fv->height * fv->width * fv->depth;
+
+  fv->contents = (float *) malloc(sizeof(float) * fvDataLen);
+
+  // Clear everything
+  for(i = 0; i < fvDataLen; i++) {
+    *(fv->contents + i) = 0;
+  }
+}
+
+void setBigPathVolumeParallel(struct FloatVolume *pv, struct FloatVolume *dv) {
+  // Initialize the empty path volume
+  makeEmptyFloatVolume(pv, dv->height, dv->width, dv->depth);
+
+  // Set the very first cell
+  *(pv->contents + 0) = *(dv->contents + 0);
+
+  // Complete x = 0, y = 0
+  setX0Y0(pv, dv);
+
+  // Complete z = 0, y = 0
+  
+
+  // Complete y = 0
+
+  
 }
 
 int main() {
